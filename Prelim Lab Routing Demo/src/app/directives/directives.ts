b2006c8e82f39bda813cec547385828f4c64b607
AdanJoshQ <adanjoshjosh@gmail.com>
2025-11-27 08:58:20 +0800

    Add files via upload

---
+++ Prelim Lab Routing Demo/src/app/directives/directives.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-directives',
+  imports: [FormsModule],
+  templateUrl: './directives.html',
+  styleUrl: './directives.css'
+})
+export class Directives {
+  // Control Properties
+  isStaticNoteVisible: boolean = false;
+  isNoteVisible: boolean = true;
+  isParagraphVisible: boolean = true;
+
+  // Methods
+  showNote() {
+    this.isNoteVisible = true;
+  }
+
+  hideNote() {
+    this.isNoteVisible = false;
+  }
+
+  toggleNote() {
+    this.isParagraphVisible = !this.isParagraphVisible;
+  }
+}
